cc9a3e93d27002af99da0342995bcd34eab38514
Marlon Santos <marlonsantos.au@gmail.com>
2020-07-16 21:39:51 +1000

    refactor fizzbuzz test

--- FizzBuzz/fizzbuzz.test.js
+++ FizzBuzz/fizzbuzz.test.js
@@ -2,16 +2,15 @@ const FizzBuzz = require('./fizzbuzz');
 
 const fizz = 'Fizz';
 const buzz = 'Buzz';
+const fb = new FizzBuzz();  
 
 test('Test fizz or buzz', () => {
-  const f = new FizzBuzz();  
-  expect(f.getFizzOrBuzz(3)).toBe(fizz);
-  expect(f.getFizzOrBuzz(5)).toBe(buzz);
+  expect(fb.getFizzOrBuzz(3)).toBe(fizz);
+  expect(fb.getFizzOrBuzz(5)).toBe(buzz);
 });
 
 test('Simulate 100 rounds and show output', () => {
-  const f = new FizzBuzz();
-  let output = f.simulate(100);
+  let output = fb.simulate(100);
   expect(output).toMatch(/\bFizz\b|\bBuzz\b/);
   console.log(output);
 });
