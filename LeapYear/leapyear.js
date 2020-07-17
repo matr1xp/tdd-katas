889f7293cb96e1399066c79449e05e4975201b3d
Marlon Santos <marlonsantos.au@gmail.com>
2020-07-17 12:01:48 +1000

    refactor leapyear function

--- LeapYear/leapyear.js
+++ LeapYear/leapyear.js
@@ -1,13 +1,6 @@
 function isLeapYear(year) {
-    if (year > 1582) {
-      if (year%4 == 0) {
-          if (year%100 == 0 && year%400 !== 0) {
-              return false;
-          }
-          return true;
-      }
-    }
-    return false;
+    'use strict';
+    return year > 1582 && year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 }
 
 module.exports = isLeapYear;
